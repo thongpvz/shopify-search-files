4b279e93220d53b2130e8be2fd9ecb5700263344
Pham Thong <thongpv@fireapps.vn>
2019-05-27 16:05:57 +0700

    update config webpack

--- webpack.prod.js
+++ webpack.prod.js
@@ -6,16 +6,16 @@ const conf = {
     mode: "production",
     devtool: false,
     plugins: [
-        new UglifyJSPlugin({
-            uglifyOptions: {
-                compress: {
-                    warnings: false,
-                    drop_console: true,
-                    drop_debugger: true
-                },
-                comments: false
-            }
-        })
+        // new UglifyJSPlugin({
+        //     uglifyOptions: {
+        //         compress: {
+        //             warnings: false,
+        //             drop_console: true,
+        //             drop_debugger: true
+        //         },
+        //         comments: false
+        //     }
+        // })
     ]
 };
 module.exports = merge(common, conf);
